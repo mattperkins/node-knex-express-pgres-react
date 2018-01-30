95585ce753ed61fbc26ca31252b2aa11b63b669f
mattperkins <github@mattperkins.com>
2018-01-30 12:59:18 +0000

    add body-parser, test get via postman

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const apiRoute = require('./routes/api')
+const bodyParser = require('body-parser')
 
 const app = express()
+
 // app.get('/', function(req,res){
 //     res.send('Hello, World!')
 // })
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 app.use('/api', apiRoute)
 
 console.log('Server running on localhost:3200')
